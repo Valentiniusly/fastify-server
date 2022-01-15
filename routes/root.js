543ce43a6de888ba0f10d56d5d430fd86bafe65c
Valentiniusly <valeprots@gmail.com>
2022-01-15 03:18:21 +0300

    feat: users and orders endpoints

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = async function (fastify) {
-  fastify.get('/', async function (request, reply) {
+  fastify.get('/users', async function (request, reply) {
+    return await fastify.db.models.user.findAll();
+  });
+  fastify.get('/orders', async function (request, reply) {
     return await fastify.db.models.order.findAll();
   });
 };
